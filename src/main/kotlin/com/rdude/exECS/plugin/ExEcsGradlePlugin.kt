7d1db948f4493d701a29afc33267d3b799f0ca17
Russian-Dude <ghritnik@gmail.com>
2022-04-17 21:02:47 +0300

    fixed plugin artifact version

--- src/main/kotlin/com/rdude/exECS/plugin/ExEcsGradlePlugin.kt
+++ src/main/kotlin/com/rdude/exECS/plugin/ExEcsGradlePlugin.kt
@@ -23,7 +23,7 @@ class ExEcsGradlePlugin : Plugin<Project>, KotlinCompilerPluginSupportPlugin {
     override fun getCompilerPluginId(): String = "execs"
 
     override fun getPluginArtifact(): SubpluginArtifact =
-        SubpluginArtifact(groupId = "com.russian-dude", artifactId = "com.russian-dude.execs-plugin", version = "1.3.1")
+        SubpluginArtifact(groupId = "com.russian-dude", artifactId = "com.russian-dude.execs-plugin", version = "1.4.0")
 
     override fun isApplicable(kotlinCompilation: KotlinCompilation<*>): Boolean = true
 }

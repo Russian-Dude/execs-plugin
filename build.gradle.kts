26b033a810f49e872a0dfc8a519f00975926b15e
Russian-Dude <ghritnik@gmail.com>
2022-09-16 19:07:35 +0300

    Version for kotlin 1.6.21

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "com.russian-dude"
-version = "1.5.0-1"
+version = "1.5.0-k1.6.21-1"
 
 repositories {
     mavenCentral()
@@ -49,7 +49,7 @@ publishing {
         create<MavenPublication>("maven") {
             groupId = "com.russian-dude"
             artifactId = "execs-plugin"
-            version = "1.5.0-1"
+            version = "1.5.0-k1.6.21-1"
             from(components["java"])
         }
     }
